2c8b2a3679de9b2e55439fa9a3090f7ac3681fc9
Ismail <ghvh8176@gmail.com>
2025-10-27 15:58:46 -0500

    detect cycle problen

---
+++ linked list/2 detectCycle.js
@@ -0,0 +1,25 @@
+const detectCycle = function(head) {
+    if(!head || !head.next){
+        return null
+    }
+
+    let slow = head.next
+    let fast = head.next.next
+
+    while(fast && fast.next){
+        slow = slow.next
+        fast = fast.next.next
+        if(fast == slow){
+            let point1 = slow
+            let point2 = head
+            while(point1 !== point2){
+              point1 =  point1.next
+                point2 = point2.next
+            }
+            return point1
+        }
+    }
+    return null
+};
+
+//DON'T FORGET TO MOVE POINTS LIKE: point = point.next 
