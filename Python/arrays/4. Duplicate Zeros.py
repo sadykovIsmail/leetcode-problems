0a2f8f40fc9c63716fee68e7f4d2ec628bc10a73
Ismail <ghvh8176@gmail.com>
2025-11-13 17:17:27 -0600

    duplicate zeros

---
+++ Python/arrays/4. Duplicate Zeros.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        i = 0
+        while i < len(arr):
+            if arr[i] == 0:
+                arr.insert(i, 0)
+                arr.pop()
+                i += 2
+            else:
+                i += 1
+        return arr
+    
+sol = Solution()
+
+print(sol.duplicateZeros([2, 2, 7, 0, 9, 9]))
+        
