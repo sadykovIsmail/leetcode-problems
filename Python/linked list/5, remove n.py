0f3a3de0629d8406ad3b7fdb567949dd2eac7574
Ismail <ghvh8176@gmail.com>
2025-12-18 19:52:07 -0600

    test

--- Python/linked list/5, remove n.py
+++ Python/linked list/5, remove n.py
@@ -12,20 +12,36 @@ class Solution:
             return None
         if not head.next:
             return None
-        
-        currentA = head
-        i = 0
-        j = 0
+        head_node = head
+        currentA = head_node
+        i = 1
+        j = 1
         while currentA.next:
             currentA = currentA.next
             i += 1
             
         point = i - n
         if point == 0:
-            head = head.next
+            return head_node.next
         currentB = head
         while j < point:
             j += 1
             currentB = currentB.next
         currentB.next = currentB.next.next
-        return head
+        return head_node
+    
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node4 = ListNode(4)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+head = node1
+n = 2
+
+test = Solution()
+
+print(test.removeNthFromEnd(head, n))
