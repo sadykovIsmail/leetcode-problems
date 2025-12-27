5ed30df7d00f82f055492cb1da32a5c6ea14286d
Ismail <ghvh8176@gmail.com>
2025-12-26 19:33:25 -0600

    connect them together

--- Python/linked list/double linked list/1. design list.py
+++ Python/linked list/double linked list/1. design list.py
@@ -40,6 +40,11 @@ node2 = ListNode(2)
 node3 = ListNode(3)
 node4 = ListNode(4)
 
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+
 obj = MyLinkedList()
 param_1 = obj.get(index)
 # obj.addAtHead(val)
