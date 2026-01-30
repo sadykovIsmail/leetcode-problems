dfe265ccd7ea5d0394b94f70f43a89b949efa02b
Ismail <ghvh8176@gmail.com>
2026-01-29 23:35:52 -0600

    final answer

--- Python/4. hash table/1. design hashset.py
+++ Python/4. hash table/1. design hashset.py
@@ -15,7 +15,10 @@ class MyHashSet:
        return
 
     def remove(self, key: int) -> None:
-        
+        if self.hash_table.get(key, False):
+            del self.hash_table[key]
+            return
+        return
  
         
 
@@ -28,7 +31,25 @@ class MyHashSet:
 # Your MyHashSet object will be instantiated and called as such:
 obj = MyHashSet()
 obj.add(2)
-# obj.remove(key)
-param_3 = obj.contains(1)
+obj.add(3)
+
+obj.remove(2)
+param_3 = obj.contains(3)
 
 print(param_3)
+
+# 1 testcase didn't pass so anser is 
+class MyHashSet:
+
+    def __init__(self):
+        self.hash_table = dict()  # store keys as dictionary keys
+
+    def add(self, key: int) -> None:
+        self.hash_table[key] = True   # mark key as present
+
+    def remove(self, key: int) -> None:
+        if key in self.hash_table:
+            del self.hash_table[key]
+
+    def contains(self, key: int) -> bool:
+        return key in self.hash_table
