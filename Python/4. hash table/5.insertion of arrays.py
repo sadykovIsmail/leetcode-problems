947f8f308f83014f44fd9a782ccb720379a55ab7
Ismail <ghvh8176@gmail.com>
2026-01-30 20:33:06 -0600

    actual answer

--- Python/4. hash table/5.insertion of arrays.py
+++ Python/4. hash table/5.insertion of arrays.py
@@ -14,3 +14,11 @@ class Solution:
     
 example = Solution()
 print(example.intersection([1, 2, 3, 4, 5], [3, 2, 6, 7, 8, 9]))
+
+#best answer
+
+from typing import List
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1) & set(nums2))
