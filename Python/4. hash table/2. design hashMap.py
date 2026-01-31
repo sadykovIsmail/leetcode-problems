06a81b1147c1c41241d338bc0aa12ec8c67dc552
Ismail <ghvh8176@gmail.com>
2026-01-30 18:55:34 -0600

    actual anwer

--- Python/4. hash table/2. design hashMap.py
+++ Python/4. hash table/2. design hashMap.py
@@ -28,3 +28,16 @@ print(obj.put(1, "b"))
 print(obj.get(1))
 obj.remove(2)
 
+# class MyHashMap:
+
+#     def __init__(self):
+#         self.data = [-1] * 1_000_001
+
+#     def put(self, key: int, value: int) -> None:
+#         self.data[key] = value
+
+#     def get(self, key: int) -> int:
+#         return self.data[key]
+
+#     def remove(self, key: int) -> None:
+#         self.data[key] = -1
