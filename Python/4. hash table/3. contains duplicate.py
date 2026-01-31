ede47ecea58b20b029c2052c359d5870ee21f4e0
Ismail <ghvh8176@gmail.com>
2026-01-30 19:22:21 -0600

    complete problem

--- Python/4. hash table/3. contains duplicate.py
+++ Python/4. hash table/3. contains duplicate.py
@@ -1,3 +1,9 @@
+from typing import List
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        
+        self.hashSet = set()
+        for num in nums:
+            if num in self.hashSet:
+                return True
+            self.hashSet.add(num)
+        return False
