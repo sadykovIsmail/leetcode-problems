6a876663e73edc727ab9f8fc8ec45b38de7a89d6
Ismail <ghvh8176@gmail.com>
2026-01-31 22:01:55 -0600

    hash map basic

--- Python/4. hash table/insight.py
+++ Python/4. hash table/insight.py
@@ -6,8 +6,8 @@ Underhoood:
  hash function: ex: value % 5 = key
  store to dic as key value 
 
- 
- # 1. initialize the hash set
+ #Hast Set
+# 1. initialize the hash set
 hashset = set() 
 # 2. add a new key
 hashset.add(3)
@@ -27,4 +27,33 @@ print("are in the hash set.")
 # 7. clear the hash set
 hashset.clear()                         
 print("Size of hashset:", len(hashset))
+
+ #Hash Map
+
+ # 1. initialize a hash map
+hashmap = {0 : 0, 2 : 3}
+# 2. insert a new (key, value) pair or update the value of existed key
+hashmap[1] = 1
+hashmap[1] = 2
+# 3. get the value of a key
+print("The value of key 1 is: " + str(hashmap[1]))
+# 4. delete a key
+del hashmap[2]
+# 5. check if a key is in the hash map
+if 2 not in hashmap:
+    print("Key 2 is not in the hash map.")
+# 6. both key and value can have different type in a hash map
+hashmap["pi"] = 3.1415
+# 7. get the size of the hash map
+print("The size of hash map is: " + str(len(hashmap)))
+# 8. iterate the hash map
+for key in hashmap:
+    print("(" + str(key) + "," + str(hashmap[key]) + ")", end=" ")
+print("are in the hash map.")
+# 9. get all keys in hash map
+print(hashmap.keys())
+# 10. clear the hash map
+hashmap.clear();
+print("The size of hash map is: " + str(len(hashmap)))
+
 """
