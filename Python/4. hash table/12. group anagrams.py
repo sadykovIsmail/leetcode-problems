4eb8e937f6dbe97e4bc62db7a1d61b66bf6a5b86
Ismail <ghvh8176@gmail.com>
2026-02-06 19:36:44 -0600

    finish the problem

--- Python/4. hash table/12. group anagrams.py
+++ Python/4. hash table/12. group anagrams.py
@@ -2,9 +2,23 @@ from typing import List
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hashmap = dict()
-        
+        for st in strs:
 
-example = 'ate'
-example2 = 'eat'
-print(sorted(example) == sorted(example2))
-print(example, example2)
+            sorted_st = "".join(sorted(st))
+            if sorted_st in hashmap.keys():
+                hashmap[sorted_st].append(st)
+            else:
+                hashmap[sorted_st] = [st]
+        return list(hashmap.values())
+
+
+example = Solution()
+print(example.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+
+# exa = 'eat'
+# sorted_exa = "".join(sorted(exa))
+# hashmap = dict()
+# hashmap[sorted_exa] = exa
+# exa2 = 'tea'
+# sorted_exa2 = "".join(sorted(exa2))
+# print()
