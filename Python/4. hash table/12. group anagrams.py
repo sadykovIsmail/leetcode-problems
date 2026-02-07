587378b35736c765c6462992e80e96f1aa2176ea
Ismail <ghvh8176@gmail.com>
2026-02-06 19:05:09 -0600

    group anagrams problem

---
+++ Python/4. hash table/12. group anagrams.py
@@ -0,0 +1,10 @@
+from typing import List
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = dict()
+        
+
+example = 'ate'
+example2 = 'eat'
+print(sorted(example) == sorted(example2))
+print(example, example2)
