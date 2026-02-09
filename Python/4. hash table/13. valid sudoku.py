8583268b16736b7b598573256282e377cc7a9ec9
Ismail <ghvh8176@gmail.com>
2026-02-08 20:55:28 -0600

    valid sudoku

--- Python/4. hash table/13. valid sudoku.py
+++ Python/4. hash table/13. valid sudoku.py
@@ -1,9 +1,93 @@
 from typing import List
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        pass
+        unique_raw = dict()
+        unique_quarter1 = {1: set(), 2: set(), 3: set()}
+        unique_quarter2 = {1: set(), 2: set(), 3: set()}
+        unique_quarter3 = {1: set(), 2: set(), 3: set()}
+        for i_col, column in enumerate(board):
+            unique = set()
 
 
-exa = dict()
-exa[1] = {1, 2, 3, 3}
-print(exa)
+
+            for i_it, item in enumerate(column):
+                if item != '.':
+                    #check for each column unique
+                    if item in unique:
+                        return False
+                    elif item not in unique:
+                        unique.add(item)
+                    #check for each row unique
+                    if i_it in unique_raw.keys():
+                        if item in unique_raw[i_it]:
+                            return False
+                        else:
+                            unique_raw[i_it].add(item)
+                    elif i_it not in unique_raw.keys():
+                        unique_raw[i_it] = set(item)
+
+                    #check for each quarted unique
+                    if i_col <= 2:
+                        if i_it <= 2:
+                            if item in unique_quarter1[1]:
+                                return False
+                            else:
+                                unique_quarter1[1].add(item)
+                        elif i_it <= 5:
+                            if item in unique_quarter1[2]:
+                                return False
+                            else:
+                                unique_quarter1[2].add(item)
+                        elif i_it <= 8:
+                            if item in unique_quarter1[3]:
+                                return False
+                            else:
+                                unique_quarter1[3].add(item)
+
+                    elif i_col <= 5:
+                        if i_it <= 2:
+                            if item in unique_quarter2[1]:
+                                return False
+                            else:
+                                unique_quarter2[1].add(item)
+                        elif i_it <= 5:
+                            if item in unique_quarter2[2]:
+                                return False
+                            else:
+                                unique_quarter2[2].add(item)
+                        elif i_it <= 8:
+                            if item in unique_quarter2[3]:
+                                return False
+                            else:
+                                unique_quarter2[3].add(item)
+
+                    elif i_col <= 8:
+                        if i_it <= 2:
+                            if item in unique_quarter3[1]:
+                                return False
+                            else:
+                                unique_quarter3[1].add(item)
+                        elif i_it <= 5:
+                            if item in unique_quarter3[2]:
+                                return False
+                            else:
+                                unique_quarter3[2].add(item)
+                        elif i_it <= 8:
+                            if item in unique_quarter3[3]:
+                                return False
+                            else:
+                                unique_quarter3[3].add(item)
+
+
+
+
+
+
+
+        print(unique_raw)
+        return True
+
+
+exa = Solution()
+test = [[".",".",".",".","5",".",".","1","."],[".","4",".","3",".",".",".",".","."],[".",".",".",".",".","3",".",".","1"],["8",".",".",".",".",".",".","2","."],[".",".","2",".","7",".",".",".","."],[".","1","5",".",".",".",".",".","."],[".",".",".",".",".","2",".",".","."],[".","2",".","9",".",".",".",".","."],[".",".","4",".",".",".",".",".","."]]
+print(exa.isValidSudoku(test))
